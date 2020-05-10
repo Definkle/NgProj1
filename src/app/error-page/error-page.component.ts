c57c73538ec7a2c208c1913346548edd9a5488c7
Definkle <52527078+Definkle@users.noreply.github.com>
2020-05-10 16:31:56 +0800

    code refactor

--- src/app/error-page/error-page.component.ts
+++ src/app/error-page/error-page.component.ts
@@ -1,28 +1,25 @@
-import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute, Data } from '@angular/router';
-import { Subscription } from 'rxjs';
+import { Component, OnInit } from "@angular/core";
+import { ActivatedRoute, Data } from "@angular/router";
+import { Subscription } from "rxjs";
 
 @Component({
-  selector: 'app-error-page',
-  templateUrl: './error-page.component.html',
-  styleUrls: ['./error-page.component.css']
+  selector: "app-error-page",
+  templateUrl: "./error-page.component.html",
+  styleUrls: ["./error-page.component.css"],
 })
 export class ErrorPageComponent implements OnInit {
   errorMessage: string;
   private paramsSubscription: Subscription;
 
-  constructor(private route: ActivatedRoute) { }
+  constructor(private route: ActivatedRoute) {}
 
   ngOnInit(): void {
-    this.paramsSubscription = this.route.data.subscribe(
-      (data: Data) => {
-        this.errorMessage = data['message'];
-      }
-    );
+    this.paramsSubscription = this.route.data.subscribe((data: Data) => {
+      this.errorMessage = data["message"];
+    });
   }
-  
+
   ngOnDestroy() {
     this.paramsSubscription.unsubscribe();
   }
-
 }
